c82827b8e4fb1649a18a5df877c1616cfb5f53d1
smart94423 <smart94423@gmail.com>
2023-08-20 20:33:34 +0200

    failed workaround attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
+import { cjsInterop } from 'vite-plugin-cjs-interop'
 
 const config: UserConfig = {
-  plugins: [react(), ssr()]
+  plugins: [
+    react(),
+    ssr(),
+    cjsInterop({
+      dependencies: ['lexical']
+    })
+  ],
+  ssr: {
+    external: ['@mdxeditor/editor', 'lexical']
+  }
 }
 
 export default config
